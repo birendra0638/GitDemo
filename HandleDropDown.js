17775e9e29e6a2c71b25a00e19d137bd3183cb6f
birendra <bindhanibk@gmail.com>
2018-09-28 16:05:03 +0530

    indian Architect commited

--- HandleDropDown.js
+++ HandleDropDown.js
@@ -38,6 +38,8 @@ describe("How to play with dropdown", function() {
 								console.log(columnText);
 								console.log("columnText1");
 								console.log("columnText2");
+								console.log("columnText3");
+								console.log("columnText4");
 							})
 				})
 
